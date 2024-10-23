ed06ffeb43d62010d119a0e574d447a7b5b970ae
unknown <maksoleksyuk@gmail.com>
2024-10-23 13:51:53 +0300

    fixed home icon

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -9,7 +9,7 @@ export const BreadCrumbs: React.FC<Props> = ({ pageName }) => {
   return (
     <div className="bread-crumbs">
       <Link to="/" className="bread-crumbs__icon">
-        <img src="/img/svg/home.svg" alt="home" />
+        <img src="img/svg/home.svg" alt="home" />
       </Link>
       <div className="bread-crumbs__arrow">
         <img src="/img/svg/arrow-left.svg" alt="arrow-left" />
